8ed55d9c87b17fa236aa02771b0dd8b9a74dcbf4
SamMansch <sammansch@rocketmortgage.com>
2021-10-21 16:30:47 -0400

    update recipe img

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'this is simply a test', 'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/10brisket.jpg'),
-    new Recipe('Another Test Recipe', 'this is simply a test', 'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/10brisket.jpg')
+    new Recipe('Another Stew Test Recipe', 'this is simply a test', 'https://www.jessicagavin.com/wp-content/uploads/2020/12/beef-stew-14-1200.jpg')
   ];
 
   constructor() { }
