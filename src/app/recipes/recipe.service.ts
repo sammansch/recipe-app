48a51f3ff044ec78107dd10ee02a9291ec31ebb1
SamMansch <s.mansch@gmail.com>
2021-10-27 13:06:31 -0400

    add shopping list service
    refactored code to add new service. Shopping list service also allows for the addition of new ingredients.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'this is simply a test', 'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/10brisket.jpg'),
         new Recipe('Another Stew Test Recipe', 'this is simply a test', 'https://www.jessicagavin.com/wp-content/uploads/2020/12/beef-stew-14-1200.jpg')
